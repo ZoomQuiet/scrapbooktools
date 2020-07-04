5d068b658cedc466e81fbe02e506285a4cc894a6
ZoomQuiet <zoomquiet+git@gmail.com>
2020-07-04 16:31:57 +0800

    🎨 UPDATE: append _ann working

--- append_ann.py
+++ append_ann.py
@@ -31,6 +31,10 @@ class Borg():
     AIM = 'index.html'
     INC = 'append.html'
 
+    TAG0 = '::...'
+    TAG1 = '...::'
+    TAGb = '</body>'
+
 
 
 # init all cfg. var
@@ -39,7 +43,7 @@ CF = Borg()
 
 def append(troot,sroot,bname):
     #print(troot,sroot,bname)
-    print(CF.AIM)
+    #print(CF.AIM)
     _inc = '%s/%s'%(troot, CF.INC)
     _pages = '%s/%s'%(sroot, CF.PAGE)
     #print(_inc, _pages)
@@ -47,16 +51,78 @@ def append(troot,sroot,bname):
     _inc_htm = open(_inc,'r').read()
     #print(_inc_htm)
     _aim_pages = _page_walker(sroot)
-    _replace_inc(_aim_pages)
+    print('\n replace ScrapBook-s-pages in', bname)
+    _replace_inc(_aim_pages,sroot,_inc_htm)
     return None
-def _replace_inc(subs):
-    print(len(subs),subs[0])
+
+
+def _replace_inc(subs,sroot,_inc):
+    #print(len(subs),subs[0])
+    #print('subs[0]', subs[0], _aim)
+    for p in subs:
+        _aim = '{}/{}/{}/{}'.format(sroot
+                , CF.PAGE
+                , p
+                , CF.AIM)
+        if os.path.exists(_aim):
+            _htm = open(_aim,'r').readlines()
+            print(_aim)
+            _exp = ''
+            isTAG0 = 0
+            isTAG1 = 0
+            noTAG = 0
+            for l in _htm:
+                if CF.TAG0 == l[:5]:
+                    #print('got>>>',CF.TAG0)
+                    isTAG0 = 1
+                    _exp += l
+                elif CF.TAG1 == l[:5]:
+                    #print('got<<<',CF.TAG1)
+                    isTAG1 = 1
+                    _exp += _inc
+                    _exp += l
+                elif CF.TAGb in l:
+                    #print('TAGb ~> ',l)
+                    if isTAG0 or isTAG1:
+                        pass
+                    else:
+                        noTAG = 1
+                        _exp += '\n%s\n'%CF.TAG0
+                        _exp += _inc
+                        _exp += '\n%s\n'%CF.TAG1
+                        _exp += l
+                else:
+                    if isTAG0:
+                        pass
+                    else:
+                        _exp += l
+
+            #print(_exp)
+            open(_aim,'w').write(_exp)
+
+
+
+
+
+
+
+
+
+        else:
+            continue
+    
     return None
+    
+    
+
+
+
+
 def _page_walker(sroot):
     _pages = '%s/%s'%(sroot, CF.PAGE)
-    print(_pages)
+    #print(_pages)
     _subs = os.listdir(_pages)
-    print(len(_subs),_subs[0])
+    #print(len(_subs),_subs[0])
     
     return _subs
 
