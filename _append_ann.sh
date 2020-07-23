cfd60897315b8b8768b7ad6cdfb9a27328ad35ca
ZoomQuiet <zoomquiet+git@gmail.com>
2020-07-23 23:17:47 +0800

    🎨 UPDATE: upd for 11py

---
+++ _append_ann.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#=========================================================== action bath
+
+#=========================================================== action bath
+
+python append_ann.py ../zqCoder
+python append_ann.py ../ZqDevRel
+python append_ann.py ../ZqFLOSS
+python append_ann.py ../ZqSKM
+python append_ann.py ../zqDevRes
+python append_ann.py ../zqPythonic
+python append_ann.py ../zqSCM
+python append_ann.py ../zqSMM
+
+
+#=========================================================== action DONE
+#exit  0
